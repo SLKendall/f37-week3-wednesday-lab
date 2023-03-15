ebf1ff636489acf2b5080b88160b748fba4b9b93
Spencer Kendall <spencerlkendall@gmail.com>
2023-03-15 17:14:14 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+console.log("hello world")
+
+const message = document.querySelector(`#message`)
+
+const addMovie = (event) => {
+    event.preventDefault()
+
+    let inputField = document.querySelector(`input`)
+    
+    let movie = document.createElement(`li`)
+    
+    let movieTitle = document.createElement(`span`)
+
+    movieTitle.textContent = inputField.value
+
+    movieTitle.addEventListener(`click`, crossOffMovie)
+
+    movie.appendChild(movieTitle)
+
+    let deleteBtn = document.createElement(`button`)
+
+    deleteBtn.textContent = "x"
+
+    deleteBtn.addEventListener(`click`, deleteMovie)
+
+    movie.appendChild(deleteBtn)
+
+    document.querySelector(`ul`).appendChild(movie)
+
+    inputField.value = ""
+}
+
+const deleteMovie = (event) => {
+    event.target.parentNode.remove()
+    message.textContent = "-Movie removed-"
+}
+
+let formSubmit = document.querySelector(`form`)
+formSubmit.addEventListener(`submit`, addMovie)
+
+const crossOffMovie = (event) => {
+    event.target.classList.toggle(`checked`)
+    if (event.target.classList.contains(`checked`)){
+        message.textContent = "-Movie watched-"
+    } else {
+        message.textContent = "-Movie readded-"
+    }
+}
